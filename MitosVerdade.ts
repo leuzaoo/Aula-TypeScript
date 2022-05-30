be3685b9f04b65a4580c218e11507dee9481a450
leuzaoo <leuzaoocontato@gmail.com>
2022-05-30 14:20:59 -0300

    Mitos-e-Verdades

---
+++ MitosVerdade.ts
@@ -0,0 +1,55 @@
+// "Vou ter que aprender tudo de novo!" - Mito
+
+// A linguagem TS é muito semelhante ao JS, porém com algumas implementações
+// mas na hora de codar é igual. Coisas que terão que aprender:
+// Códigos específicos do TS.
+
+
+
+// "Vou ter que reescrever tudo para TS." - Mito
+
+// Podemos ter um trabalho específico feito em JS, e podemos deixá-lo
+// vivendo em JS, se formos precisar mexer no arquivo, aí é legal
+// passar para TS. Mas isso não é uma regra. Depende da sua equipe.
+
+
+
+// "Já escrevo teste, não preciso disso."
+
+// Existem alguns tipos de testes que no JS não irá funcionar, então
+// temos o TS para que possa auxiliar quando isso acontecer.
+
+
+
+// "Só funciona com programação orientada a objeto." - Mito
+
+// O TS funciona muito bem com programação funcional e não somente
+// a objeto.
+
+
+
+// "Ele é verboso demais, precisa tipar tudo!" - Mito
+
+// De fato o código será um pouco maior porque terá que definir os 
+// tipos, mas isso não te obrigado a botar "tipo" em tudo.
+
+// O TS quando já sabe o que é aquele retorno, variável ou resultado,
+// não é necessário "tipar" ele.
+
+
+
+// "Só serve pra projeto grande!" - Mito
+
+// Não faz diferença se é pra grandes ou pequenos projetos, a verdade
+// é que o TS fica bem mais organizado, ajuda a encontrar possíveis
+// erros mais facilmente. 
+
+
+
+// "É só usar PropTypes que dá na mesma" - Mito
+
+// O PropTypes não nos bloqueia códigos, não dá erros, o máximo que
+// acontece é dar "WARNING" no 'console' e vida que segue.
+
+// O TypeScript te impede de fazer alguma coisa errada, do que só ter
+// ter um "WARNING" na tela.
